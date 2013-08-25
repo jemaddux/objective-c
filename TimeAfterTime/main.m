fe325e3b78874061dcaa62831fd667f595f949bc
John Maddux <jemaddux@gmail.com>
2013-08-25 18:40:17 -0400

    Initial Commit

---
+++ TimeAfterTime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by John Maddux on 8/25/13.
+//  Copyright (c) 2013 John Maddux. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

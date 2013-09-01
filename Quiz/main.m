23cb2930a8de88468b530e99ade1c39f8f24b603
John Maddux <jemaddux@gmail.com>
2013-09-01 15:14:14 -0400

    Initial Commit

---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by John Maddux on 9/1/13.
+//  Copyright (c) 2013 John Maddux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

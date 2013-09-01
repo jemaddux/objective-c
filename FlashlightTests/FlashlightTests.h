7e4ab53a7ad3c5875bfe8c2b0ab14ed68be3fcb4
John Maddux <jemaddux@gmail.com>
2013-09-01 12:31:36 -0400

    Initial Commit

---
+++ FlashlightTests/FlashlightTests.h
@@ -0,0 +1,13 @@
+//
+//  FlashlightTests.h
+//  FlashlightTests
+//
+//  Created by John Maddux on 9/1/13.
+//  Copyright (c) 2013 John Maddux. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FlashlightTests : SenTestCase
+
+@end
---
+++ FlashlightTests/FlashlightTests.m
@@ -0,0 +1,32 @@
+//
+//  FlashlightTests.m
+//  FlashlightTests
+//
+//  Created by John Maddux on 9/1/13.
+//  Copyright (c) 2013 John Maddux. All rights reserved.
+//
+
+#import "FlashlightTests.h"
+
+@implementation FlashlightTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FlashlightTests");
+}
+
+@end
